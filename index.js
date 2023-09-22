760ea16e4f1609fd067909637484681d1282f864
vipul353 <thakurbipul353@gmail.com>
2023-09-22 14:29:06 +0530

    corse removed

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const express = require('express');
-const Data = require('./Data/data');
-const router = require('./routers/AllRouters');
-const cors = require('cors');
-const app = express()
-app.use(cors())
-app.get('/',(req,res)=>{
-res.send('app is running')
-})
-app.use('/api',router)
+const express = require("express");
+const Data = require("./Data/data");
+const router = require("./routers/AllRouters");
+const cors = require("cors");
+const app = express();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.get("/", (req, res) => {
+  res.send("app is running");
+});
+app.use("/api", router);
 
-app.listen(3000,()=>{
-    console.log('app running on port 3000');
-})
+app.listen(3000, () => {
+  console.log("app running on port 3000");
+});
